6c52a124da6351e893c9ec1b552888988cbe212f
Magic-80 <developper.magic@gmail.com>
2025-06-18 15:14:06 +0200

    Feat: - Mise en place du projet

--- App.tsx
+++ App.tsx
@@ -5,24 +5,49 @@
  * @format
  */
 
-import { NewAppScreen } from '@react-native/new-app-screen';
-import { StatusBar, StyleSheet, useColorScheme, View } from 'react-native';
+import React, { useEffect } from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import SplashScreen from './src/components/SplashScreen';
+import Accueil from './src/components/Accueil';
+import ApiPage from './src/components/ApiPage';
+import AffichageDonnees from './src/components/AffichageDonnees';
+import { initDatabase, closeDatabase } from './src/services/migrations/index.js';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 function App() {
-  const isDarkMode = useColorScheme() === 'dark';
+  useEffect(() => {
+    // Ouvrir la base de données au montage de l'application
+    initDatabase()
+      .then(() => console.log('Database initialized successfully.'))
+      .catch(error => console.error('Failed to initialize database:', error));
+
+    // Fermer la base de données à la fin de vie de l'application (optionnel, mais bonne pratique)
+    return () => {
+      closeDatabase();
+    };
+  }, []); // Le tableau vide assure que cela ne s'exécute qu'une fois
 
   return (
-    <View style={styles.container}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <NewAppScreen templateFileName="App.tsx" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash">
+        <Stack.Screen
+          name="Splash"
+          component={SplashScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Accueil"
+          component={Accueil}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen name="ApiPage" component={ApiPage} options={{ title: 'Page API' }} />
+        <Stack.Screen name="AffichageDonnees" component={AffichageDonnees} options={{ title: 'Données internes' }} />
+
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
 
 export default App;
